b8a26d3bae3c68783ff4b2d645a694a69dfee450
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-05-14 10:24:13 +0100

    new linter added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
 
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
@@ -8,31 +13,29 @@ import Home from './components/Home'
 
 import './style.scss'
 
-class App extends React.Component {
-
-  render(){
-    return (
-      <BrowserRouter>
-        <div>
-          <header>
-            <Link to="/">
-              <h1>Quiz App</h1>
-            </Link>
-            <nav>
-              <Link to="/register">Register</Link>
-              <Link to="/login">Login</Link>
-            </nav>
-          </header>
-          <Switch>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">
+            <h1>Quiz App</h1>
+          </Link>
+          <nav>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
+
 
 ReactDOM.render(
   <App />,
