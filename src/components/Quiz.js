cbad387789d2dee0668de992b0fc5d8b0ec5ad45
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-05-16 13:53:20 +0100

    answers form submitting and setting answers to state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,13 +2,48 @@ import React, { Component } from 'react'
 
 
 class Quiz extends Component {
+  state={
+    answers: [],
+    newAnswer: {}
+  }
 
 
+  handleChange = ({ target: { name, value }}) => {
+    console.log(value, 'value on change')
+    const { newAnswer } = this.state
+    this.setState({ newAnswer: {...newAnswer, [name]: value }})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { answers, newAnswer } = this.state
+    this.setState({ answers: [...answers, newAnswer]})
+  }
 
 
   render(){
-    return(
-      <h2>Girls Day Quiz</h2>
+    console.log(this.state, 'I AM STATE')
+    return (
+      <div>
+        <h2> Girls Day Quiz</h2>
+        <div className="questions">
+          <h3>Question 1</h3>
+          <p>Who said this?</p>
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+            placeholder="Answers here.."
+            type="text"
+            name="answer"
+            onChange={this.handleChange}
+            className="answers-input"
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+      </div>
     )
   }
 }
